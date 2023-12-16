337bc5653ef5c14e79de3bd8558ed46d3b0cde27
alibaykara1 <153395582+alibaykara1@users.noreply.github.com>
2023-12-16 14:10:20 +0300

    düzeltilmiş kod

--- src/yeniKodlar/githubDeneme.java
+++ src/yeniKodlar/githubDeneme.java
@@ -4,7 +4,7 @@ public class githubDeneme {
     public static void main(String[] args) {
 
         System.out.println("Ali");
-        System.out.priln("Ali");
+        System.out.println("Ali");
 
     }
 }
