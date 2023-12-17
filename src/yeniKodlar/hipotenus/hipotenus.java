0ecf0184e8153abf78cb350965419b995496fe96
alibaykara1 <153395582+alibaykara1@users.noreply.github.com>
2023-12-17 17:04:46 +0300

    diküçghipo

--- src/yeniKodlar/hipotenus/hipotenus.java
+++ src/yeniKodlar/hipotenus/hipotenus.java
@@ -1,4 +1,25 @@
-package yeniKodlar.hipotenus;
 
+//Java ile kullanıcıdan dik kenarlarının uzunluğunu alan ve hipotenüsü hesaplayan programı yaz
+// a ve b dik kenarlar c hipotenüs
+package yeniKodlar.hipotenus;
+import java.util.Scanner;
 public class hipotenus {
+    public static void main(String[] args) {
+    int a,b;
+    double c;
+    Scanner input = new Scanner (System.in);
+        System.out.print("A kenarını giriniz : ");
+        a = input.nextInt();
+
+        System.out.print("B kenarını giriniz :");
+        b = input.nextInt();
+
+        c =Math.sqrt((a*a)+(b*b));
+
+        System.out.print("Hipotenüs :"+c);
+
+
+
+
+    }
 }
