94b967bf4010854b58434a2a797a2bda3f9f33bc
alibaykara1 <153395582+alibaykara1@users.noreply.github.com>
2023-12-24 11:49:29 +0300

    faktöriyel hesabı

---
+++ src/yeniKodlar/faktoriyel/faktoriyel.java
@@ -0,0 +1,21 @@
+//javada girilen sayının faktöriyelini hesaplama
+// n! =1*2*3*4*...*n
+
+package yeniKodlar.faktoriyel;
+import java.util.Scanner ;
+public class faktoriyel{
+    public static void main(String[] args) {
+
+        int n,total = 1;
+
+        Scanner inp = new Scanner (System.in);
+        System.out.print("Sayıyı giriniz :");
+        n = inp.nextInt();
+
+        for(int i=1 ;i<=n ;i++){
+            total*=i;
+        }
+        System.out.print(n+". faktöriyel :"+total);
+
+    }
+}
